72951ff3207eaf68f1f9683f676c72b09b2e1b2f
shafu <selfouly@gmail.com>
2023-09-08 13:54:23 +0200

    swap

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,64 @@ use std::fmt;
 struct Opcode {
     byte: String, 
     name: String, 
+    operand_size: usize, // in bytes
 }
 
 impl Opcode {
-    fn new(byte: String, name: String) -> Opcode {
-        Opcode { byte, name }
+    fn new(byte: String) -> Opcode {
+        Opcode { byte, name: "NOP".to_string(), operand_size: 0 }
     }
 }
 
 impl fmt::Display for Opcode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}", self.byte, self.name)
+        write!(f, "{} {} {}", self.byte, self.name, self.operand_size)
     }
 }
 
+fn byte_to_push(opcode: &mut Opcode) -> &mut Opcode {
+    let dec = usize::from_str_radix(&opcode.byte, 16).unwrap();
+    let pushes = dec - 0x5F;
+    opcode.operand_size = pushes;
+    opcode.name = "PUSH".to_owned() + &pushes.to_string();
+    opcode
+}
+
+fn byte_to_dup(opcode: &mut Opcode) -> &mut Opcode {
+    let dec = usize::from_str_radix(&opcode.byte, 16).unwrap();
+    let dups = dec - 0x7F;
+    opcode.operand_size = dups;
+    opcode.name = "DUP".to_owned() + &dups.to_string();
+    opcode
+}
+
+fn byte_to_swap(opcode: &mut Opcode) -> &mut Opcode {
+    let dec = usize::from_str_radix(&opcode.byte, 16).unwrap();
+    let swaps = dec - 0x8F;
+    opcode.operand_size = swaps;
+    opcode.name = "SWAP".to_owned() + &swaps.to_string();
+    opcode
+}
+
 fn byte_to_opcode(byte: &String) -> Opcode {
-    let mut opcode = Opcode::new(byte.to_string(), "NOP".to_string());
+    let mut opcode = Opcode::new(byte.to_string());
     match byte.as_str() {
-        "60" => opcode.name = "PUSH1".to_string(),
-        "80" => opcode.name = "DUP1".to_string(),
-        _ => (),
+        "00" => opcode.name = "STOP".to_string(),
+        "01" => opcode.name = "ADD".to_string(),
+        "02" => opcode.name = "MUL".to_string(),
+        "03" => opcode.name = "SUB".to_string(),
+        s => {
+            let dec = usize::from_str_radix(&s, 16).unwrap();
+            if dec >= 0x5F && dec <= 0x7F {
+                byte_to_push(&mut opcode);
+            } 
+            if dec >= 0x80 && dec <= 0x8F {
+                byte_to_dup(&mut opcode);
+            }
+            if dec >= 0x90 && dec <= 0x9F {
+                byte_to_swap(&mut opcode);
+            }
+        }
     }
     opcode
 }
