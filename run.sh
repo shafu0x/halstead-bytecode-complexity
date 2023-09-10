41a29b930d85277f4af73afad27481a912b08d72
shafu <selfouly@gmail.com>
2023-09-10 19:35:23 +0200

    refactor

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-# cargo run /home/shafu/halstead/bytecode.txt
-cargo run /Users/shafu/halstead/bytecode-large.txt --rm-metadata
+cargo run /home/shafu/halstead/bytecode-large.txt --rm-metadata
+# cargo run /Users/shafu/halstead/bytecode-large.txt --rm-metadata
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 use std::env;
 use std::fs;
+use std::io;
 
 mod opcode;
 use opcode::Opcode;
@@ -16,24 +17,24 @@ fn strip_metadata(bytecode: &String) -> String {
 }
 
 // read the bytecode from the file
-fn read_bytecode() -> String {
+fn read_bytecode() -> Result<String, io::Error> {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
-    let mut bytecode = fs::read_to_string(path).expect("Something went wrong reading the file");
+    let mut bytecode = fs::read_to_string(path)?;
     // remove 0x if it exists
     if &bytecode[0..2] == "0x" {
         bytecode = bytecode[2..].to_string();
     }
     // remove metadata
-    if args.iter().any(|arg| arg == METADATA_FLAG) {
+    if args.len() > 2 && &args[2] == METADATA_FLAG {
         bytecode = strip_metadata(&bytecode);
     }
 
-    bytecode
+    Ok(bytecode)
 }
 
 fn main() {
-    let bytecode = read_bytecode();
+    let bytecode = read_bytecode().expect("Error reading bytecode");
 
     let mut i = 0;
 
@@ -88,12 +89,13 @@ fn main() {
         .collect::<HashSet<_>>()
         .len();
 
-    let mut unique_operands = HashSet::new();
-    for op in operands {
-        unique_operands.insert(op);
-    }
+    let unique_operands = operands
+        .iter()
+        .map(|operand| operand)
+        .collect::<HashSet<_>>()
+        .len();
 
-    let vocabulary = unique_opcodes + unique_operands.len();
+    let vocabulary = unique_opcodes + unique_operands;
     println!("Vocabulary: {}", vocabulary);
 
     let length = number_of_operations + number_of_operands;
@@ -102,8 +104,8 @@ fn main() {
     let volume = length as f64 * (vocabulary as f64).log2();
     println!("Volume:     {:.2}", volume);
 
-    let difficulty = (unique_opcodes as f64) / 2.0 * (number_of_operands as f64)
-        / (unique_operands.len() as f64);
+    let difficulty =
+        (unique_opcodes as f64) / 2.0 * (number_of_operands as f64) / (unique_operands as f64);
     println!("Difficulty: {:.2}", difficulty);
 
     let effort = difficulty * volume;
