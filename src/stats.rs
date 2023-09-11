f7af5cd470b1bebf543f7d99d574722309159559
shafu <selfouly@gmail.com>
2023-09-11 12:29:38 +0200

    refactor

--- src/stats.rs
+++ src/stats.rs
@@ -1,30 +1,27 @@
 use crate::opcode::Opcode;
-use crate::operand::Operand;
 use std::collections::HashSet;
 
 pub struct Stats {
     pub opcodes: Vec<Opcode>,
-    pub operands: Vec<Operand>,
-    pub opcode_count: usize,
-    pub operand_count: usize,
 }
 
 impl Stats {
     pub fn new() -> Self {
         Self {
             opcodes: Vec::new(),
-            operands: Vec::new(),
-            opcode_count: 0,
-            operand_count: 0,
         }
     }
 
     pub fn add_opcode(&mut self, opcode: Opcode) {
-        self.operand_count += opcode.stack_input_size;
-        self.opcode_count += 1;
-        // TODO: refactor
-        self.operands.push(opcode.operand.clone());
         self.opcodes.push(opcode);
+    }
+
+    pub fn get_opcodes_with_operand(&self) -> Vec<Opcode> {
+        self.opcodes
+            .iter()
+            .filter(|opcode| opcode.has_operand)
+            .cloned()
+            .collect()
     }
 
     pub fn count_unique_opcodes(&self) -> usize {
@@ -36,9 +33,9 @@ impl Stats {
     }
 
     pub fn count_unique_operands(&self) -> usize {
-        self.operands
+        self.get_opcodes_with_operand()
             .iter()
-            .map(|operand| &operand.value)
+            .map(|opcode| &opcode.operand.value)
             .collect::<HashSet<_>>()
             .len()
     }
@@ -48,18 +45,19 @@ impl Stats {
 
         let unique_opcodes = self.count_unique_opcodes();
         let unique_operands = self.count_unique_operands();
+        let total_operands = self.get_opcodes_with_operand().len();
 
         let vocabulary = unique_opcodes + unique_operands;
         println!("Vocabulary: {}", vocabulary);
 
-        let length = self.opcodes.len() + self.operands.len();
+        let length = self.opcodes.len() + total_operands;
         println!("Length:     {}", length);
 
         let volume = length as f64 * (vocabulary as f64).log2();
         println!("Volume:     {:.2}", volume);
 
         let difficulty =
-            (unique_opcodes as f64) / 2.0 * (self.operands.len() as f64) / (unique_operands as f64);
+            (unique_opcodes as f64) / 2.0 * (total_operands as f64) / (unique_operands as f64);
         println!("Difficulty: {:.2}", difficulty);
 
         let effort = difficulty * volume;
