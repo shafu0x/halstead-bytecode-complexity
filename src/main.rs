aa6c3ada0de94d528f0f84913a1a64b12ff5e64b
shafu <selfouly@gmail.com>
2023-09-12 20:06:50 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,18 @@ fn main() {
         std::process::exit(1);
     }
 
-    let directory = &args[1];
+    let dir = &args[1];
     let remove_metadata = args.contains(&METADATA_FLAG.to_string());
     let verbose = args.contains(&VERBOSE_FLAG.to_string());
 
-    match fs::read_dir(directory) {
+    match fs::read_dir(dir) {
         Ok(files) => {
             for file in files {
                 let path = file.unwrap().path();
                 let path_str = &path.to_str().unwrap().to_string();
                 println!("{}", path_str);
-                let mut disassembler = Disassembler::new(path_str, remove_metadata);
 
+                let mut disassembler = Disassembler::new(path_str, remove_metadata);
                 while let Ok(opcode) = disassembler.next_opcode() {
                     if verbose {
                         println!("{:>5}: {}", disassembler.line_number, opcode);
