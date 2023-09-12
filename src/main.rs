be3573562405e1d51d4cb24a831e44d6d1390cb0
shafu <selfouly@gmail.com>
2023-09-12 19:47:27 +0200

    verbose flag

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,26 @@ mod stats;
 use disassembler::Disassembler;
 
 const METADATA_FLAG: &str = "--rm-metadata";
+const VERBOSE_FLAG: &str = "--v";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        eprintln!("Usage: {} <file_path> [--strip-metadata]", args[0]);
+        eprintln!("Usage: {} <file_path> [{}] [{}]", args[0], VERBOSE_FLAG, METADATA_FLAG);
         std::process::exit(1);
     }
 
     let path = &args[1];
-    let strip_metadata = args.contains(&METADATA_FLAG.to_string());
+    let remove_metadata = args.contains(&METADATA_FLAG.to_string());
+    let verbose = args.contains(&VERBOSE_FLAG.to_string());
 
-    let mut disassembler = Disassembler::new(path, strip_metadata);
+    let mut disassembler = Disassembler::new(path, remove_metadata);
 
     while let Ok(opcode) = disassembler.next_opcode() {
-        println!("{:>5}: {}", disassembler.line_number, opcode);
+        if verbose {
+            println!("{:>5}: {}", disassembler.line_number, opcode);
+        }
     }
 
     disassembler.print_stats();
